6179475cecb8789e857776067a28811427fe3ca5
wojkub <wk303204@student.polsl.pl>
2024-06-26 08:17:38 +0200

    pasta-add formatowanie

--- WebClient/src/app/pasta-add/pasta-add.component.ts
+++ WebClient/src/app/pasta-add/pasta-add.component.ts
@@ -3,7 +3,7 @@ import { ChangeDetectorRef, Component } from '@angular/core';
 @Component({
   selector: 'app-pasta-add',
   templateUrl: './pasta-add.component.html',
-  styleUrl: './pasta-add.component.css'
+  styleUrl: './pasta-add.component.css',
 })
 export class PastaAddComponent {
   constructor(private cdr: ChangeDetectorRef) {}
