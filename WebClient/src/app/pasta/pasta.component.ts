6dbe192c3b915dd24d2359129c356b62928f09e2
wojkub <wk303204@student.polsl.pl>
2024-06-26 08:18:21 +0200

    pasta formatowanie

--- WebClient/src/app/pasta/pasta.component.ts
+++ WebClient/src/app/pasta/pasta.component.ts
@@ -3,13 +3,13 @@ import { ChangeDetectorRef, Component } from '@angular/core';
 @Component({
   selector: 'app-pasta',
   templateUrl: './pasta.component.html',
-  styleUrl: './pasta.component.css'
+  styleUrl: './pasta.component.css',
 })
 export class PastaComponent {
   constructor(private cdr: ChangeDetectorRef) {}
   showTextComponent: string = 'true';
   toggleComponent(value: string) {
     this.showTextComponent = value;
-    this.cdr.detectChanges(); 
+    this.cdr.detectChanges();
   }
 }
