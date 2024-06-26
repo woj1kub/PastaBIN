eeee24b28ab9e8a00b1b5a162f1ec5635c119d71
wojkub <wk303204@student.polsl.pl>
2024-06-26 08:23:09 +0200

    login format

--- WebClient/src/app/login/login.component.ts
+++ WebClient/src/app/login/login.component.ts
@@ -7,29 +7,38 @@ import { NgForm } from '@angular/forms';
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrl: './login.component.css'
+  styleUrl: './login.component.css',
 })
 export class LoginComponent {
   invalidLogin: boolean = false;
 
-  constructor(private http: HttpClient, private router: Router, private authService : AuthService) {}
+  constructor(
+    private http: HttpClient,
+    private router: Router,
+    private authService: AuthService
+  ) {}
 
   login(form: NgForm) {
     const credentials = JSON.stringify(form.value);
-    this.http.post("https://localhost:7023/login", credentials, {
-      headers: new HttpHeaders({
-        "Content-Type": "application/json"
+    this.http
+      .post('https://localhost:7023/login', credentials, {
+        headers: new HttpHeaders({
+          'Content-Type': 'application/json',
+        }),
       })
-    }).subscribe(response  => {
-      const token = (response as any).token;
-      localStorage.setItem("jwt", token);
-      this.invalidLogin = false;
-      this.router.navigate(["/pasty"]);
-    }, err => {
-      this.invalidLogin = true;
-    });
-    }
-  
+      .subscribe(
+        (response) => {
+          const token = (response as any).token;
+          localStorage.setItem('jwt', token);
+          this.invalidLogin = false;
+          this.router.navigate(['/pasty']);
+        },
+        (err) => {
+          this.invalidLogin = true;
+        }
+      );
+  }
+
   IsUserAuthenticated(): boolean {
     return this.authService.IsUserAuthenticated();
   }
